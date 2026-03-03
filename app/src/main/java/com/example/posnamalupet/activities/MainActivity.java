b0ba23db85c3ca8ca2dbc14c569c59c2195375b5
playerone-bit <codexalpha24@gmail.com>
2026-03-03 15:15:28 +0800

    have comment for debugging database

--- app/src/main/java/com/example/posnamalupet/activities/MainActivity.java
+++ app/src/main/java/com/example/posnamalupet/activities/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
 //        DebugSomething();
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
